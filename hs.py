18a0f0188aea6011e0ac6f9649a5257dfb6a50d4
Babakhatra <84274457+Babakhatra@users.noreply.github.com>
2021-06-04 14:13:50 +0545

    Create hs.py

---
+++ hs.py
@@ -0,0 +1,14 @@
+love("\033[1;91m«---------------\033[1;96mHAPPY BIRTHDAY\033[1;91m---------------»")
+        love("\033[1;96m«-----------------Disclaimer---------------»")
+        love("\033[1;91m✾●~~~~~●●✦CONGRATULATION✦●●~~~~~●✾")
+        love("\033[1;93m❍❍❍❍❍~~~~~~❍❍❍✬✥✬❍❍❍~~~~~~❍❍❍❍❍")
+        love("\033[1;93m៚ HAPPY BIRTHDAY")
+        love("\033[1;93m៚ HAPPY BIRTHDAY")
+        love("\033[1;93m៚ HAPPY BIRTHDAY")
+        love("\033[1;93m៚ HAPPY BIRTHDAY")
+        love("\033[1;93m៚ HAPPY BIRTHDAY")
+        love("\033[1;91m«---------------- HAPPY BIRTHDAY --------------»")
+        love("\033[1;95m✾●~~~●●✦CONGRATS✦●●~~~●✾")
+        love("\033[1;96m «-----------------\033[1;92mRAHUL MISHRA\033[1;96m--------------»")
+
+
